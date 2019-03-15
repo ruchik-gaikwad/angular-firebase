9c0f3b215545dd83ecb464a31cd49d54ec7b4a26
ruchik.gaikwad <ruchik.gaikwad@stackroute.in>
2019-03-15 22:05:34 +0530

    get your firebase key here git add .! :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
+import * as uuid from "uuid";
+import { map  } from 'rxjs/operators';
 
 
 @Component({
@@ -10,13 +12,26 @@ import {AngularFireDatabase} from '@angular/fire/database';
 export class AppComponent {
 
   constructor(private db:AngularFireDatabase ) {
-      db.list('/').valueChanges().subscribe(data => {
+      // db.list('/').valueChanges().subscribe(data => {
+      //   console.log(data)
+      // })
+      // db.list('/').push({
+      //   'name': "Ruchik Gaikwad", 
+      //   "Session": "Afternoon",
+      //   "id": uuid.v4()
+      // })
+      console.log(uuid.v4());
+      let itemRef = db.list('/');
+      let items = itemRef.snapshotChanges().pipe(map(changes => changes.map(c => ({key: c.payload.key, ...c.payload.val()}))))
+
+      console.log()
+      items.subscribe(data => {
         console.log(data)
       })
-      db.list('/').push({
-        'name': "Ruchik", 
-        "Session": "Afternoon"
-      })
+      // db.list('/', ref => ref.orderByChild('id').equalTo("d2f7a434-1148-4dd1-9af9-807b644521e3")).valueChanges().subscribe(console.log)
+      // db.list('/', ref => ref.orderByChild('id').equalTo("d2f7a434-1148-4dd1-9af9-807b644521e3")).valueChanges().
+    
   }
   title = 'firebase-angular';
+
 }
